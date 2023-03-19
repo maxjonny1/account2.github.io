6e4c4db3744888e329b95e20531eb215cfa7bdd1
maxjonny1 <128029914+maxjonny1@users.noreply.github.com>
2023-03-19 10:40:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ function toggleReveal($hideElement, $showElement){
         $('button').click(function(){
           $('button').parent().addClass('active');
           setTimeout(function(){
-            $('button').addClass('failed');
+            $('button').addClass('success');
           }, 3400);
           setTimeout(function(){
             alert("Your payment has been confirmed and your account has been credited succesfully!");
             $('button').parent().removeClass('active');
-            $('button').removeClass('failed');
+            $('button').removeClass('success');
           }, 4200);
         });
       });
